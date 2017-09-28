ec19c02f761905d491091ec8a87eef6d89f90294
M. Toyokura <32252611+MToyokura@users.noreply.github.com>
2017-09-28 16:20:17 +0900

    Update GomiPy openpyxl サンプル.py

--- source/model/GomiPy openpyxl サンプル.py
+++ source/model/GomiPy openpyxl サンプル.py
@@ -6,13 +6,8 @@ from openpyxl import load_workbook
 wb = load_workbook('Python リサイクル市 会計用.xlsx')
 ws2 = wb["raw"]
 
-
-#item_num = int(input())
-
-item_num = 17004
-
+item_num = int(input())
 item_num_col = ws2['A']
-
 
 for i in item_num_col:
     if i.value == item_num:
@@ -20,10 +15,8 @@ for i in item_num_col:
         item_cell_coordinate = item_cell_string[12:-1]
         item_cell_row = item_cell_string[13:-1]
 
-
 tuple1 = ws2['A' + item_cell_row:'E' + item_cell_row]
 
 for j in tuple1:
     for k in j:
         print (k.value)
-    
