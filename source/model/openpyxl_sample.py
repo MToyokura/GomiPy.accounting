97a20c9d01579373d870321b11edfb8af371a910
MkT2phage <32252611+MkT2phage@users.noreply.github.com>
2017-09-30 11:28:26 +0900

    辞書の中に辞書を入れる構造

--- source/model/openpyxl_sample.py
+++ source/model/openpyxl_sample.py
@@ -1,5 +1,5 @@
-# openpyxlに関する情報 http://openpyxl.readthedocs.io/en/default/tutorial.html
-# このコードは各商品データを辞書型に格納する場合を想定したもの
+## openpyxlに関する情報 http://openpyxl.readthedocs.io/en/default/tutorial.html
+## このコードは各商品データを辞書型に格納する場合を想定したもの
 
 
 from openpyxl import load_workbook
@@ -7,11 +7,9 @@ from openpyxl import load_workbook
 wb = load_workbook('Python リサイクル市 会計用.xlsx')
 ws2 = wb["raw"]
 
-
 info_keys = ['serialNum', 'name', 'initPrice', 'altPrice', 'remaining',
              'height', 'width', 'depth', 'company', 'model',
              'year', 'notes', 'web']
-
 
 item_num = int(input('商品番号を入力してください: '))
 item_num_col = ws2['A']
@@ -25,25 +23,18 @@ for i in item_num_col:
         # item_cell_coordinate は 'A204' のような文字と数字を用いたセルの名前
         # item_cell_row は '204' のような行の名前
 
-
 info_values = []
 for j in ws2[item_cell_row]:
     info_values.append(j.value)
 
-
-item_dictionary = dict(zip(info_keys, info_values))
-print(item_dictionary)
-
-
-
-
-
-
+item_info_dict = dict(zip(info_keys, info_values))
+item_dict = {item_num: item_info_dict}
+print(item_dict)
 
 '''
-# クラス利用の想定
-# 大量のクラスインスタンスを作るより辞書に格納するほうが好ましいらしい
-class Item:
+## クラス利用の想定
+## 大量のクラスインスタンスを作るより辞書に格納するほうが好ましいらしい
+class item:
     def __init__(self, serialNum, name, initPrice, altPrice, remaining,
                  height, width, depth, company, model, year, notes, web):
         self.serialNum = serialNum
@@ -59,16 +50,4 @@ class Item:
         self.year = year
         self.notes = notes
         self.web = web
-'''
-
-
-
-
-'''
-
-item_info = ws2['A' + item_cell_row:'E' + item_cell_row]
-
-for j in item_info:
-    for k in j:
-        print (k.value)
 '''
