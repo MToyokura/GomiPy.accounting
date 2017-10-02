e64f0e8c8a334916cdaede0fcbdc45b2e64b4a96
ykiu <32252655+ykiu@users.noreply.github.com>
2017-10-02 19:50:23 +0900

    シートごとの保存のバグ修正
    元のコードが実は保存していないだけだったので修正。

--- source/model/purchased_item_manager.py
+++ source/model/purchased_item_manager.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from openpyxl import load_workbook
+from itertools import islice
 
 class PurchasedItemManager:
 
@@ -58,14 +60,36 @@ class PurchasedItemManager:
         '''
         return self.dataframe[self.dataframe['会計番号'] == customer_id]
 
+def convert_openpyxl_to_pandas(ws):
+    '''
+    openpyxl の worksheet を、pandas の dataframe に変換します。
+
+    https://openpyxl.readthedocs.io/en/default/pandas.html
+    のコピペです。
+    '''
+    data = ws.values
+    cols = next(data)[1:]
+    data = list(data)
+    idx = [r[0] for r in data]
+    data = (islice(r, 1, None) for r in data)
+    df = pd.DataFrame(data, index=idx, columns=cols)
+    return df
+
 if __name__ == '__main__':
     '''
     このファイルが単体で実行されている場合は、if文内のスクリプトが実行されます。
 
     逆に他のスクリプトから import されている場合は、以下のコードは実行されません。
     '''
-    file = pd.ExcelFile('purchased_items.xlsx')
-    worksheet = file.parse('Sheet1')
+
+    # 特定のシートの書き込む方法は下記を参照。
+    # https://stackoverflow.com/a/20221655
+    file = load_workbook('purchased_items.xlsx')
+    writer = pd.ExcelWriter('purchased_items.xlsx', engine='openpyxl')
+    writer.book = file
+    writer.sheets = dict((ws.title, ws) for ws in file.worksheets)
+
+    worksheet = convert_openpyxl_to_pandas(file.get_sheet_by_name('Sheet1'))
     purchased_item_manager = PurchasedItemManager(worksheet)
 
     print('読み込み完了')
@@ -76,5 +100,5 @@ if __name__ == '__main__':
     print('filter_by_customer()で特定の会計番号の商品を絞り込み')
     print(purchased_item_manager.filter_by_customer(1))
     print('元のファイルに上書き保存')
-    writer = pd.ExcelWriter('purchased_items.xlsx')
     purchased_item_manager.dataframe.to_excel(writer, 'Sheet1')
+    writer.save()
